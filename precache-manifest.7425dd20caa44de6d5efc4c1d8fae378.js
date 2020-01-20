8551b39d0585ec3cea78e527e7ffaabf2a054f72
Aliesa Jackson <aliesaj2010@yahoo.com>
2020-01-20 18:46:47 -0500

    Updates

---
+++ precache-manifest.7425dd20caa44de6d5efc4c1d8fae378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d80f9d8511f64b67244e34bf40072b",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "bc2c102d75c2bc0ceda1",
+    "url": "/clicky_game/static/css/main.3d1416ce.chunk.css"
+  },
+  {
+    "revision": "238eaa412f3204f6dc3f",
+    "url": "/clicky_game/static/js/2.d964dec8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky_game/static/js/2.d964dec8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc2c102d75c2bc0ceda1",
+    "url": "/clicky_game/static/js/main.d7ef23cb.chunk.js"
+  },
+  {
+    "revision": "d2c0f891696b1657df43",
+    "url": "/clicky_game/static/js/runtime-main.2a365fda.js"
+  }
+]);
